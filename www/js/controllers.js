a22f122070b0ad4fadd3f7decc892e2f996394e7
dayuoba <taddayu@sina.com>
2015-04-19 20:18:30 +0800

    add ui

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,34 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, Crons) {
+	$scope.add = function(index) {
+		console.log('hello');
+		console.log(index);
+	}
+	$scope.name = 'hi';
+	//add virtual datas here
+	$scope.crons = Crons;
+	console.log(Crons);
+	//alert(Conrs);
+})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('ChatsCtrl', function($scope, $http, Chats) {
+	$scope.chats = Chats.all();
+	//console.log($http);
+	$http.get('https://cnodejs.org/api/v1/topics').success(function(data) {
+		console.log(data.data[0]);
+	});
+	$scope.remove = function(chat) {
+		Chats.remove(chat);
+	}
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+	$scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+	$scope.settings = {
+		enableFriends: true
+	};
 });
